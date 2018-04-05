7f2011f0177b97a9f0c171f6dba4690ed5fabfec
Jasna Stankovic <jasnaa994@gmail.com>
2018-04-05 19:40:55 +0200

    Add empty main activity.

---
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/MainActivity.java
@@ -0,0 +1,19 @@
+package rs.ac.uns.ftn.pma.event_organizer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+    }
+}
