98a5c582483ac1637f086fc039c0e5694518c4c6
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2018-04-18 00:37:29 +0200

    Login to Register navigation

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/LoginActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/LoginActivity.java
@@ -1,10 +1,13 @@
 package rs.ac.uns.ftn.pma.event_organizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 import rs.ac.uns.ftn.pma.event_organizer.R;
 
@@ -17,6 +20,19 @@ public class LoginActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(myToolbar);
+
+        Button registerBtn = findViewById(R.id.register);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    private void openRegisterActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/RegisterActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/RegisterActivity.java
@@ -1,7 +1,9 @@
 package rs.ac.uns.ftn.pma.event_organizer;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
     }
 }
