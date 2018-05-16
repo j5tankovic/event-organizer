ea318f029eb66e23df97dc088cf895220845fc2b
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2018-05-16 21:55:29 +0200

    Registration changed to firebase auth

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/RegisterActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/RegisterActivity.java
@@ -1,13 +1,19 @@
 package rs.ac.uns.ftn.pma.event_organizer.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +27,7 @@ import rs.ac.uns.ftn.pma.event_organizer.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "RegisterActivity";
     private DatabaseReference databaseReference;
     private FirebaseDatabase firebaseDatabase;
     private TextView textView_username;
@@ -28,8 +35,6 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView textView_email;
     private TextView textView_firstname;
     private TextView textView_lastname;
-
-    private Button button_register;
 
     private FirebaseAuth mAuth;
 
@@ -50,8 +55,6 @@ public class RegisterActivity extends AppCompatActivity {
         textView_firstname = findViewById(R.id.first_name);
         textView_lastname = findViewById(R.id.last_name);
 
-        button_register = findViewById(R.id.btn_register);
-
         firebaseDatabase = FirebaseDatabase.getInstance();
         // get reference to 'users' node
         databaseReference = firebaseDatabase.getReference("users");
@@ -63,22 +66,96 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onRegisterClick(View view){
-        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
-        createUser();
-    }
-
-    private void createUser(){
-
-        //String userId = databaseReference.push().getKey();
-        //System.out.println("\n\nUSER ID: " + userId);
-        User newUser = new User(
-                0l,
+        Toast.makeText(this, "Clicked on Register Button", Toast.LENGTH_LONG).show();
+        createAccount(
                 textView_username.getText().toString(),
                 textView_password.getText().toString(),
                 textView_email.getText().toString(),
                 textView_firstname.getText().toString(),
                 textView_lastname.getText().toString()
         );
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = textView_email.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            textView_email.setError("Required.");
+            valid = false;
+        } else {
+            textView_email.setError(null);
+        }
+
+        String password = textView_password.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            textView_password.setError("Required.");
+            valid = false;
+        } else {
+            textView_password.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void sendEmailVerification() {
+        // Disable button
+        findViewById(R.id.btn_register).setEnabled(false);
+
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(RegisterActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createAccount(final String username, final String password, final String email, final String firstname, final String lastname) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            createUser(username,password,email,firstname,lastname);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void createUser(String username, String password, String email, String firstname, String lastname){
+
+        User newUser = new User(0l, username, password, email, firstname, lastname);
 
         System.out.println("USER : " + newUser.toString());
         databaseReference.child(textView_username.getText().toString()).setValue(newUser);
@@ -91,10 +168,8 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
     }
 
 }
