105ae322f0ed7436973789551865d7be7f071c11
iv17 <ivana.unitedforce@gmail.com>
2018-05-23 23:11:08 +0200

    event image upload

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/AddNewEventActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/AddNewEventActivity.java
@@ -1,15 +1,30 @@
 package rs.ac.uns.ftn.pma.event_organizer.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,16 +37,44 @@ public class AddNewEventActivity extends AppCompatActivity {
 
     public static final String ADDED_ITEM = "rs.ac.uns.ftn.pma.event_organizer.ADDED_ITEM";
 
+    private TextView name;
+    private TextView description;
+    private TextView start_date;
+    private TextView end_date;
+    private TextView budget;
+
+    private Button upload;
+    private ImageView uploadedPicture;
+
+    private Button add;
+
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private String eventPicturePath;
+
     private DatabaseReference databaseReference;
     private FirebaseDatabase firebaseDatabase;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_event);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("events");
+        storageReference = FirebaseStorage.getInstance().getReference();
 
-        Button add = findViewById(R.id.add_new_event);
+        name = findViewById(R.id.new_event_name);
+        description = findViewById(R.id.new_event_description);
+        start_date = findViewById(R.id.new_event_start_date);
+        end_date = findViewById(R.id.new_event_end_date);
+        budget = findViewById(R.id.new_event_budget);
+
+        upload = findViewById(R.id.new_event_upload_image);
+        uploadedPicture = findViewById(R.id.new_event_image);
+
+        add = findViewById(R.id.add_new_event);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,22 +84,15 @@ public class AddNewEventActivity extends AppCompatActivity {
             }
         });
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("events");
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
     }
 
     private Event formEvent() {
-        TextView name = findViewById(R.id.new_event_name);
-        TextView description = findViewById(R.id.new_event_description);
-        TextView start_date = findViewById(R.id.new_event_start_date);
-        TextView end_date = findViewById(R.id.new_event_end_date);
-        TextView budget = findViewById(R.id.new_event_budget);
-
-        System.out.println(name.getText().toString());
-        System.out.println(description.getText().toString());
-        System.out.println(start_date.getText().toString());
-        System.out.println(end_date.getText().toString());
-
         Event event = new Event();
         event.setName(name.getText().toString());
         event.setDescription(description.getText().toString());
@@ -90,6 +126,68 @@ public class AddNewEventActivity extends AppCompatActivity {
 
         event.setId(key);
         databaseReference.child(key).setValue(event);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                uploadedPicture.setImageBitmap(bitmap);
+                uploadImage();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            eventPicturePath = "images/events/"+ name.getText().toString()+".jpg";
+            StorageReference ref = storageReference.child(eventPicturePath);
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(AddNewEventActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(AddNewEventActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 
 }
