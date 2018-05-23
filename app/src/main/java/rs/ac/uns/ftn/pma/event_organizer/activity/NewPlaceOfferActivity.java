b2e21585bb97a019822217869bcaad150cb698cd
Jasna Stankovic <jasnaa994@gmail.com>
2018-05-23 11:08:22 +0200

    Add buttons for accepting and rejecting place offer.

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/NewPlaceOfferActivity.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/activity/NewPlaceOfferActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,6 +26,7 @@ import rs.ac.uns.ftn.pma.event_organizer.model.ShoppingItem;
 
 public class NewPlaceOfferActivity extends AppCompatActivity {
 
+    public static final String TAG = NewPlaceOfferActivity.class.getSimpleName();
     public static final String ADDED_OFFER = "rs.ac.uns.ftn.pma.event_organizer.ADDED_OFFER";
 
     private DatabaseReference databaseReference;
@@ -43,6 +49,23 @@ public class NewPlaceOfferActivity extends AppCompatActivity {
                 PlaceOffer offer = formPlaceOffer();
                 save(offer);
                 formResult(offer);
+            }
+        });
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
             }
         });
 
