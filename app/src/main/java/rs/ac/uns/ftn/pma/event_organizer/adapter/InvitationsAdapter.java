0ff4808ee58d219ef81327401ecbc7e157e7fb4c
svetlana <stojadinov.svetlana@gmail.com>
2018-05-25 19:59:01 +0200

    accepting/rejecting invitation - do: updating database

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/adapter/InvitationsAdapter.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/adapter/InvitationsAdapter.java
@@ -7,14 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
 
 import rs.ac.uns.ftn.pma.event_organizer.R;
 import rs.ac.uns.ftn.pma.event_organizer.model.Invitation;
+import rs.ac.uns.ftn.pma.event_organizer.model.enums.InvitationStatus;
 
 public class InvitationsAdapter extends ArrayAdapter<Invitation> {
     private Context context;
@@ -27,16 +30,43 @@ public class InvitationsAdapter extends ArrayAdapter<Invitation> {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
+    public View getView(final int position, View view, ViewGroup parent) {
 
         if(view==null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             view = inflater.inflate(R.layout.activity_invitation_list_view, null, true);
         }
         Invitation inv=getItem(position);
+        final int pos=position;
         if(inv!=null){
             TextView name = (TextView) view.findViewById(R.id.invitation_name);
             TextView date = (TextView) view.findViewById(R.id.invitation_date);
+            final ImageButton button_acc=(ImageButton)view.findViewById(R.id.invitation_status_acc);
+            final ImageButton button_rej=(ImageButton)view.findViewById(R.id.invitation_status_rej);
+
+            if(inv.getStatus().equals(InvitationStatus.ACCEPTED)){
+                button_acc.setImageResource(R.drawable.ic_check_green_24dp);
+           }else if (inv.getStatus().equals(InvitationStatus.REJECTED)){
+                button_rej.setImageResource(R.drawable.ic_close_red_24dp);
+            }
+
+            button_acc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    button_acc.setImageResource(R.drawable.ic_check_green_24dp);
+                    button_rej.setImageResource(R.drawable.ic_close_gray_24dp);
+                    //set and update database
+                }
+            });
+
+            button_rej.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    button_rej.setImageResource(R.drawable.ic_close_red_24dp);
+                    button_acc.setImageResource(R.drawable.ic_check_gray_24dp);
+                    //set and update database
+                }
+            });
 
             name.setText(inv.getEvent().getName());
             String date1str="";
