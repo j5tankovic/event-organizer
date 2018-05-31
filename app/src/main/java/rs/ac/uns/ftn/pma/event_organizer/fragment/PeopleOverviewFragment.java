e8ce8877ffe95f7a4c5d4caa2cac0064efeef5bb
svetlana <stojadinov.svetlana@gmail.com>
2018-05-31 12:05:54 +0200

    sending email if user doesn't have profile, new invitation: autocompliting for email

--- app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/fragment/PeopleOverviewFragment.java
+++ app/src/main/java/rs/ac/uns/ftn/pma/event_organizer/fragment/PeopleOverviewFragment.java
@@ -1,5 +1,7 @@
 package rs.ac.uns.ftn.pma.event_organizer.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
@@ -9,6 +11,8 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +26,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -116,23 +121,52 @@ public class PeopleOverviewFragment extends Fragment {
         });
 
         ImageButton button=(ImageButton)view.findViewById(R.id.new_invitation_email_button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textViewEmail=(TextView)view.findViewById(R.id.new_invitation_email_edittext);
+                AutoCompleteTextView textViewEmail=(AutoCompleteTextView) view.findViewById(R.id.new_invitation_email_edittext);
                 String email=(textViewEmail).getText().toString();
 
                 if(TextUtils.isEmpty(email)) {
                     textViewEmail.setError("Required");
                 } else {
-                    Toast.makeText(getContext(), "Inviting " + email, Toast.LENGTH_LONG).show();
-                    textViewEmail.setText("");
+                   textViewEmail.setText("");
 
                     User foundedUser = findUserByEmail(email);
 
                     if (foundedUser == null) {
-                        Toast.makeText(getContext(), email + " doesn't have a profile", Toast.LENGTH_LONG).show();
-                        //IF THIS USER DOESN'T HAVE PROFILE
+                        Intent i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                                "mailto", email, null));
+                        i.putExtra(Intent.EXTRA_SUBJECT, "Invitation");
+
+                        StringBuilder sb=new StringBuilder();
+                        sb.append("Dear Friend, ");
+                        sb.append('\n');
+                        sb.append("I would like to invite you to attend to my event: ");
+                        sb.append(selectedEvent.getName());
+
+                        if(selectedEvent.getStartDateTime()!=null && selectedEvent.getEndDateTime()!=null) {
+                            sb.append(". The event will start ");
+                            sb.append( new SimpleDateFormat("dd/MM/yyyy").format(selectedEvent.getStartDateTime()));
+                            sb.append(" and end ");
+                            sb.append( new SimpleDateFormat("dd/MM/yyyy").format(selectedEvent.getEndDateTime()));
+                        }
+                        if(selectedEvent.getFinalPlace()!=null) {
+                            sb.append(" at ");
+                            sb.append(selectedEvent.getFinalPlace().getLocationName());
+                        }
+                        sb.append('\n');
+                        sb.append('\n');
+                        sb.append("Regards,");
+                        sb.append('\n');
+                        sb.append(selectedEvent.getCreator().getName()+" "+selectedEvent.getCreator().getLastName()+"!");
+                        i.putExtra(Intent.EXTRA_TEXT   , sb.toString());
+                        try {
+                            startActivity(Intent.createChooser(i, "Send mail..."));
+                        } catch (android.content.ActivityNotFoundException ex) {
+                            Toast.makeText(getContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                        }
                     } else {
                         boolean invited = false;
                         if (foundedUser.getEmail().equals(loggedUser.getEmail())) {
@@ -145,11 +179,11 @@ public class PeopleOverviewFragment extends Fragment {
                             }
                         }
                         if (!invited) {
+                            Toast.makeText(getContext(), "Inviting " + email, Toast.LENGTH_LONG).show();
                             createNewInvitation(foundedUser, selectedEvent);
                             adapter.notifyDataSetChanged();
                         }
                     }
-
                 }
             }
         });
@@ -189,8 +223,22 @@ public class PeopleOverviewFragment extends Fragment {
             newUser.setId((long) singleUser.get("id"));
             allUsers.add(newUser);
         }
+       setDropBox();
     }
 
+    private void setDropBox(){
+        String[] users = new String[allUsers.size()];
+        for(int i=0;i<allUsers.size();i++){
+            users[i]=allUsers.get(i).getEmail();
+        }
+        if(users.length!=0) {
+            ArrayAdapter<String> adapterTextView = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, users);
+            AutoCompleteTextView textView=(AutoCompleteTextView) view.findViewById(R.id.new_invitation_email_edittext);
+            textView.setAdapter(adapterTextView);
+            textView.setThreshold(1);
+        }
+
+    }
     public void getAllInvitations(Map<String,Object> invitations) {
         if (invitations != null) {
             for (Map.Entry<String, Object> entry : invitations.entrySet()) {
